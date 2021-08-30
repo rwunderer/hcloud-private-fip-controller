981aa93819413e96e756f859be06ec84d0f9864a
Robert Wunderer <robert.wunderer@creadomus.at>
2021-08-30 11:36:28 +0000

    Make gofmt happy

--- cmd/private-fip-controller/main.go
+++ cmd/private-fip-controller/main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "log"
-    "os"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/rwunderer/hcloud-private-fip-controller/internal/app/fipcontroller"
-    "github.com/rwunderer/hcloud-private-fip-controller/internal/pkg/config"
+	"github.com/rwunderer/hcloud-private-fip-controller/internal/app/fipcontroller"
+	"github.com/rwunderer/hcloud-private-fip-controller/internal/pkg/config"
 )
 
 func main() {
-    configFile := os.Getenv("CONFIG_FILE")
+	configFile := os.Getenv("CONFIG_FILE")
 
-    config := &config.Config{}
+	config := &config.Config{}
 
-    // define command-line flags
-    flag.StringVar(&config.ApiToken, "hcloud-token", os.Getenv("HCLOUD_TOKEN"), "Hetzner Cloud API Token")
-    flag.StringVar(&config.HostIp, "host-ip", os.Getenv("HOST_IP"), "IP address of local node")
-    flag.StringVar(&config.IpAddress, "ip-address", os.Getenv("IP_ADDRESS"), "Internal ip address to use as 'floating' ip")
-    flag.StringVar(&config.NetworkName, "hcloud-network", os.Getenv("HCLOUD_NETWORK"), "ID or name of the Hetzner Cloud private network")
+	// define command-line flags
+	flag.StringVar(&config.ApiToken, "hcloud-token", os.Getenv("HCLOUD_TOKEN"), "Hetzner Cloud API Token")
+	flag.StringVar(&config.HostIp, "host-ip", os.Getenv("HOST_IP"), "IP address of local node")
+	flag.StringVar(&config.IpAddress, "ip-address", os.Getenv("IP_ADDRESS"), "Internal ip address to use as 'floating' ip")
+	flag.StringVar(&config.NetworkName, "hcloud-network", os.Getenv("HCLOUD_NETWORK"), "ID or name of the Hetzner Cloud private network")
 
-    // optionally read flags from config file
-    if _, err := os.Stat(configFile); err == nil {
-        if err := config.ReadFile(configFile); err != nil {
-            log.Print(fmt.Errorf("could not parse controller config file: %v", err))
-            os.Exit(1)
-        }
-    }
+	// optionally read flags from config file
+	if _, err := os.Stat(configFile); err == nil {
+		if err := config.ReadFile(configFile); err != nil {
+			log.Print(fmt.Errorf("could not parse controller config file: %v", err))
+			os.Exit(1)
+		}
+	}
 
-    // parse command-line flags
-    flag.Parse()
+	// parse command-line flags
+	flag.Parse()
 
-    // run actual controller
-    controller, err := fipcontroller.NewController(config)
-    if err != nil {
-        log.Print(fmt.Errorf("could not initialize controller: %v", err))
-        os.Exit(1)
-    }
+	// run actual controller
+	controller, err := fipcontroller.NewController(config)
+	if err != nil {
+		log.Print(fmt.Errorf("could not initialize controller: %v", err))
+		os.Exit(1)
+	}
 
-    ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-    controller.Run(ctx)
+	controller.Run(ctx)
 }
--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,55 +1,55 @@
 package config
 
 import (
-    "fmt"
-    "io/ioutil"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"strings"
 
-    yaml "gopkg.in/yaml.v2"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type Config struct {
-    ApiToken string `yaml:"hcloudToken"`
-    HostIp string `yaml:"hostIP"`
-    IpAddress string `yaml:"ipAddress"`
-    NetworkName string `yaml:"networkName"`
+	ApiToken    string `yaml:"hcloudToken"`
+	HostIp      string `yaml:"hostIP"`
+	IpAddress   string `yaml:"ipAddress"`
+	NetworkName string `yaml:"networkName"`
 }
 
 func (conf *Config) ReadFile(inFile string) error {
-    content, err := ioutil.ReadFile(inFile)
-    if err != nil {
-        return fmt.Errorf("failed to read config file: %v", err)
-    }
+	content, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		return fmt.Errorf("failed to read config file: %v", err)
+	}
 
-    if err := yaml.Unmarshal(content, &conf); err != nil {
-        return fmt.Errorf("failed to unmarshal config file: %v", err)
-    }
+	if err := yaml.Unmarshal(content, &conf); err != nil {
+		return fmt.Errorf("failed to unmarshal config file: %v", err)
+	}
 
-    return nil
+	return nil
 }
 
 func (conf *Config) Validate() error {
-    var errs []string
+	var errs []string
 
-    if conf.ApiToken == "" {
-        errs = append(errs, "API token not provided. Please set HCLOUD_TOKEN env var!")
-    }
+	if conf.ApiToken == "" {
+		errs = append(errs, "API token not provided. Please set HCLOUD_TOKEN env var!")
+	}
 
-    if conf.HostIp == "" {
-        errs = append(errs, "Host IP not provided. Please set HOST_IP env var!")
-    }
+	if conf.HostIp == "" {
+		errs = append(errs, "Host IP not provided. Please set HOST_IP env var!")
+	}
 
-    if conf.IpAddress == "" {
-        errs = append(errs, "Floating IP address not provided. Please set IP_ADDRESS env var!")
-    }
+	if conf.IpAddress == "" {
+		errs = append(errs, "Floating IP address not provided. Please set IP_ADDRESS env var!")
+	}
 
-    if conf.NetworkName == "" {
-        errs = append(errs, "Network name not provided. Please set HCLOUD_NETWORK env var!")
-    }
+	if conf.NetworkName == "" {
+		errs = append(errs, "Network name not provided. Please set HCLOUD_NETWORK env var!")
+	}
 
-    if len(errs) > 0 {
-        return fmt.Errorf("%s", strings.Join(errs, ", "))
-    }
+	if len(errs) > 0 {
+		return fmt.Errorf("%s", strings.Join(errs, ", "))
+	}
 
-    return nil
+	return nil
 }
